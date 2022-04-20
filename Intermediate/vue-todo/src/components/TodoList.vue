c7d6e24673d3bc1068282fb4398d75f285595374
Sunjin <tjswls8062@skuniv.ac.kr>
2022-04-20 17:47:25 +0900

    first complete

---
+++ Intermediate/vue-todo/src/components/TodoList.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+        <i class="checkBtn fa-solid fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem)"></i>
+        <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fa-solid fa-trash-can"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data : function(){
+    return {
+      todoItems: []
+    }
+  },
+  methods : {
+    removeTodo : function(todoItem, index){
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function(todoItem){
+      todoItem.completed = !todoItem.completed;
+      // 로컬 스토리지의 데이터를 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    sortObj: function(a, b){   //sort 함수 선언
+      if(a.item < b.item){
+        return -1;
+      }
+      if(a.item > b.item){
+        return 1;
+      }
+      return 0;
+    }
+  },
+  created: function(){
+    if (localStorage.length > 0){
+      for (var i = 0; i < localStorage.length; i++){
+        this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+      }
+      this.todoItems.sort(this.sortObj);  //sort
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 50px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+</style>
